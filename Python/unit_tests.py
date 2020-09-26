62c8ad42ac90e401d7a90a5f8d9cae2a5d7f4ce9
José Luis Velázquez <joseluisvzg@gmail.com>
2020-09-26 13:44:42 -0500

    Unittests for exercise 2

--- Python/unit_tests.py
+++ Python/unit_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import unittest
 from Exercise1 import vowels_counter
+from Exercise2 import vowels_changer
 
 class TestExercise1(unittest.TestCase):
     def test_vowel_counter(self):
@@ -11,6 +12,16 @@ class TestExercise1(unittest.TestCase):
 
     def test_no_chars(self):
         self.assertEqual(vowels_counter(""), 0, "Should be 0")
+
+class TestExercise2(unittest.TestCase):
+    def test_vowel_changer(self):
+        self.assertEqual(vowels_changer("hello"), "hillu", "Should be 'hillu'")
+
+    def test_no_vowel(self):
+        self.assertEqual(vowels_changer("hll"), 'hll', "Should be 'hll'")
+
+    def test_no_chars(self):
+        self.assertEqual(vowels_changer(""), '', "Should be ''")
 
 if __name__ == '__main__':
     unittest.main()
